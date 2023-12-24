9ea6ef62f2236d08a0c88fc5f4682e293095d6c2
Daichi Collins <119629268+Daichi1108@users.noreply.github.com>
2023-12-24 01:07:16 -0500

    commit

--- src/Game.java
+++ src/Game.java
@@ -1,2 +1,18 @@
 public class Game {
+
+    private String answer;
+
+    public Game(String answer) {
+        this.answer = answer;
+    }
+
+    public int guess(String guess) {
+        int total = 0;
+        for (int i = 0; i < guess.length(); i++) {
+            if (answer.contains(guess.substring(i,i+1))) {
+                total++;
+            }
+        }
+        return total;
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,18 @@ import java.io.FileNotFoundException;
 
 public class Main {
     public static void main(String[] args) throws FileNotFoundException {
-        Guesser solver = new Guesser(new Dictionary().getDict());
-        System.out.println(solver.guess());
+        Dictionary dict = new Dictionary();
+        int total = 0;
+        for (int i = 0; i < 1000; i++) {
+            Game game = new Game(dict.getDict().get((int)(Math.random() * dict.getDict().size())));
+            Guesser solver = new Guesser(new Dictionary().getDict());
+            int moves = 1;
+            while (game.guess(solver.guess()) < 5) {
+                solver.addRule(solver.guess(), game.guess(solver.guess()));
+                moves++;
+            }
+            total += moves;
+        }
+        System.out.println(total / 1000.0);
     }
 }
